989921bb618252c083fb662a4fe6fb9d3a786668
tsovek <tomasz.a.sowa@gmail.com>
2016-03-22 00:54:52 +0100

    Adding hotel view, temporary not use css file from ~/lib/ (better in debugging).

--- KrakmApp/wwwroot/app/components/addHotel.ts
+++ KrakmApp/wwwroot/app/components/addHotel.ts
@@ -1,18 +1,19 @@
 ﻿import { Component } from 'angular2/core';
-import { CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/common';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import { MembershipService } from '../core/services/membershipService';
 import { NotificationService } from '../core/services/notificationService';
 import { User } from '../core/domain/user';
 import { DataService } from '../core/services/dataService';
 import { Result } from '../core/domain/result';
 import { Hotel } from '../core/domain/hotel';
+import { ANGULAR2_GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 
 @Component({
     selector: 'addHotel',
     providers: [MembershipService, NotificationService, DataService],
     templateUrl: './app/components/addHotel.html',
     bindings: [MembershipService, NotificationService, DataService],
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, ANGULAR2_GOOGLE_MAPS_DIRECTIVES]
 })
 export class AddHotel {
     private _user: User;
