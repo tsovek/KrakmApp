1d7c5dd816b72ee07006e5afa2e5d42e544193b1
tsovek <tomasz.a.sowa@gmail.com>
2016-03-31 23:54:20 +0200

    Banner entity.

---
+++ KrakmApp.Entities/Banner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KrakmApp.Entities
+{
+    public class Banner : IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
